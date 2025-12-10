762fa9133d568f35cf818f0b27f4916c571d0d17
Regin8532 <138576075+Regin8532@users.noreply.github.com>
2025-12-10 16:26:56 +0530

    changes

--- mySql.sql
+++ mySql.sql
@@ -37,8 +37,21 @@ INSERT INTO Table_Empl (empno, ename, job, mgr, hiredate, sal, comm, deptno) VAL
 (8844, 'KULDEEP', 'SALESMAN', 8698, '1991-09-08', 1500.00, 0.00, 30);
 SHOW tables;
 SELECT * FROM table_empl;
+A. Write a query to display EName and Sal of employees whose salary are greater than or equal to 2200?
 SELECT ename,sal FROM table_empl WHERE sal>=2200;
+
+B. Write a query to display details of employees who are not getting commission?
 SELECT * FROM table_empl WHERE comm IS NULL OR comm = 0.00;
+
+
+C. Write a query to display employee name and salary of those employees who dont have their salary in the range of 2500 to 4000?
 SELECT ename, sal FROM table_empl WHERE sal NOT BETWEEN 2500 AND 4000;
+
+D. Write a query to display the name, job title and salary of employees who dont have a manager?
+SELECT ename,job,sal FROM table_empl where mgr is NULL;
+	
+E. Write a query to display the name of an employee whose name contains "A" ás third alphabet?
 SELECT ename FROM Table_Empl WHERE ename LIKE '__A%';
+
+F. Write a query to display the name of an employee whose name contains "T" as the last alphabet?
 SELECT ename FROM Table_Empl WHERE ename LIKE '%T';
